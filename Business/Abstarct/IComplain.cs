ab60a3f0479667df33d46ddd5033b404f7968beb
seyhun5252 <seyhun721@gmail.com>
2022-08-16 11:07:25 +0300

    devamı

---
+++ Business/Abstarct/IComplain.cs
@@ -0,0 +1,20 @@
+﻿using DataLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstarct
+{
+    public interface IComplain
+    {
+        void Add(Complain complain);
+        void Delete(Complain complain);
+        void Update(Complain complain);
+
+        List<Complain> GetAll();
+        Complain GetById(int id);
+
+    }
+}
